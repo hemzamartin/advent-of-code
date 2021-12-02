03a3f5748ec32e9d93d5834daf8c336a4ba8d7a1
Martin Hemza <hemzamartas@gmail.com>
2021-12-02 12:49:06 +0100

    Improve day 2

--- 2021/2/part-one.js
+++ 2021/2/part-one.js
@@ -1,24 +1,19 @@
 const { input } = require('./input');
 
-const commands = [];
-
-for (let line of input) {
-  commands.push(line.split(' '));
-}
-
 let horizontal = 0;
 let vertical = 0;
 
-for (let command of commands) {
+for (let command of input) {
+  let value = parseInt(command[1], 10);
   switch (command[0]) {
     case 'forward':
-      horizontal += parseInt(command[1], 10);
+      horizontal += value;
       break;
     case 'up':
-      vertical -= parseInt(command[1], 10);
+      vertical -= value;
       break;
     case 'down':
-      vertical += parseInt(command[1], 10);
+      vertical += value;
       break;
   }
 }
--- 2021/2/part-two.js
+++ 2021/2/part-two.js
@@ -1,16 +1,10 @@
 const { input } = require('./input');
 
-const commands = [];
-
-for (let line of input) {
-  commands.push(line.split(' '));
-}
-
 let horizontal = 0;
 let vertical = 0;
 let aim = 0;
 
-for (let command of commands) {
+for (let command of input) {
   let value = parseInt(command[1], 10);
   switch (command[0]) {
     case 'forward':
