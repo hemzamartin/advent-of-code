9dbf235a8589d221591316ff3c72dc41ab6e174c
hemzamartin <hemzamartas@gmail.com>
2022-12-06 08:16:06 +0100

    Day 5

---
+++ 2022/day05/input.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const fs = require("fs");
+
+const input = fs
+  .readFileSync(path.join(__dirname, "input.txt"), "utf8")
+  .toString()
+  .trimEnd();
+
+module.exports = {
+  input,
+};
---
+++ 2022/day05/part-one.js
@@ -0,0 +1,43 @@
+const { input } = require("./input");
+
+const data = input.split("\n");
+const header = data
+  .slice(0, data.indexOf("") - 1)
+  .map((line) => line.split(""));
+
+const instructions = data.splice(data.indexOf("")).map((line) =>
+  line
+    .replace("move ", "")
+    .replace("from ", "")
+    .replace("to ", "")
+    .split(" ")
+    .map((number) => parseInt(number))
+);
+
+const numberOfStacks = 9;
+
+let stacks = [];
+for (let i = 0; i < numberOfStacks; i++) {
+  stacks.push(new Array());
+}
+for (let line of header.reverse()) {
+  for (let i = 1; i < line.length; i += 4) {
+    if (line[i] !== " ") {
+      stacks[Math.floor(i / 4)].push(line[i]);
+    }
+  }
+}
+
+for (let inst of instructions) {
+  for (let i = 0; i < inst[0]; i++) {
+    let cargo = stacks[inst[1] - 1].pop();
+    stacks[inst[2] - 1].push(cargo);
+  }
+}
+
+let output = "";
+for (let i = 0; i < numberOfStacks; i++) {
+  output += stacks[i].pop();
+}
+
+console.log(output);
