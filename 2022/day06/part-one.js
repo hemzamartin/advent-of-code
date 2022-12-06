8d3e1141e41ca677f291cdb0110ae4f12cbe8987
hemzamartin <hemzamartas@gmail.com>
2022-12-06 08:16:53 +0100

    Day 6

---
+++ 2022/day06/part-one.js
@@ -0,0 +1,19 @@
+const { input } = require("./input");
+
+let packetStart = [];
+
+let length = 0;
+
+for (let char of input) {
+  packetStart.push(char);
+  length++;
+  if (packetStart.indexOf(char) !== packetStart.lastIndexOf(char)) {
+    packetStart = packetStart.slice(packetStart.indexOf(char) + 1);
+    continue;
+  }
+  if (packetStart.length === 4) {
+    break;
+  }
+}
+
+console.log(length);
